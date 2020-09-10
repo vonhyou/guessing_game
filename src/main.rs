699741cf31df62179720f3d2fb74b8c869a08423
vonhyou <vonhyou@yeah.net>
2020-09-10 17:26:21 +0800

    first modify

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ fn main() {
 
   let mut guess = String::new();
 
-  io::stdin().read_line(&mut guess)
+  io::stdin()
+    .read_line(&mut guess)
     .expect("Failed to read!");
 
   println!("Your guess : {}", guess);
